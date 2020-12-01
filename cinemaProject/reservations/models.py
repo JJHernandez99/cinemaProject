178f5d4f26fec6469d1ac40cd35bbcdfc6a15a20
Gustavo Ignacio Carrillo <carrillo.ignaciog@gmail.com>
2020-12-01 10:34:44 -0300

    clases

--- cinemaProject/reservations/models.py
+++ cinemaProject/reservations/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Pelicula (models.Model):
+    nombre=models.CharField(max_length=30,help_text="Nombre de la pelicula")
+    duracion=models.IntegerField
