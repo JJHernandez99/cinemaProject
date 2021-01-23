c1d5edc726fa22a71cc881fbe17d6b762968aede
Gustavo Ignacio Carrillo <carrillo.ignaciog@gmail.com>
2021-01-23 09:45:57 -0300

    admin

--- cinemaProject/actualizar.py
+++ cinemaProject/actualizar.py
@@ -1,3 +1,13 @@
 import os
+from time import sleep
+import datetime
 
-os.system("python3 manage.py runscript service")
+hora_de_ejecucion = "7:00"
+
+while True:
+    hora_actual = datetime.datetime.now().strftime("%H:%M")
+    if hora_actual == hora_de_ejecucion:
+        os.system("python3 manage.py runscript service")
+        sleep(1380)
+    else:
+        sleep(30)
--- cinemaProject/reservations/admin.py
+++ cinemaProject/reservations/admin.py
@@ -1,1 +1,23 @@
+from django.contrib import admin
+from .models import Pelicula, Sala, Butaca, Proyeccion
 
+
+class PeliculasAdmin(admin.ModelAdmin):
+    list_display = ("id","nombre", "fechaComienzo", "fechaFinalizacion", "estado")
+
+class SalasAdmin(admin.ModelAdmin):
+    list_display = ("id","nombre", "estado","fila","asiento")
+
+
+class ProyeccionesAdmin(admin.ModelAdmin):
+    list_display = ("id","pelicula", "sala", "estado","fechaInicio","fechaFin")
+
+
+class ButacasAdmin(admin.ModelAdmin):
+    list_display = ("id","proyeccion", "fecha", "fila", "asiento")
+
+
+admin.site.register(Pelicula, PeliculasAdmin)
+admin.site.register(Sala, SalasAdmin)
+admin.site.register(Butaca, ButacasAdmin)
+admin.site.register(Proyeccion, ProyeccionesAdmin)
